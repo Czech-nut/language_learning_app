c32d6fcd5277332ec70b4fffdcf8b9777f7f0734
Valery Voitova <valeria.shcherrba@gmail.com>
2022-09-13 22:01:45 +0400

    added DTOs and code style check

---
+++ backend/dtos/lesson.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from pydantic import Field
+
+from backend.dtos.common import BaseModel
+
+
+class LessonMinimal(BaseModel):
+    id: int = Field()
+    name: str = Field(min_length=3, max_length=1024, example="Past Simple")
+    content: str = Field(example="Past Simple is used for past!")
+
+
+class LessonDTO(BaseModel):
+    id: int = Field()
+    completed: bool = Field(False)
+    preview: str = Field(example="https://s3-link.com/my-preview.png")
+    name: str = Field(example="Past Simple")
+
+
+class LessonsList(BaseModel):
+    streak: int = Field()
+    lessons: List[LessonDTO] = Field([])
